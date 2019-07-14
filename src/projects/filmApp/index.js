4fa0a1ba3d4024dc74453e876d2316f4721d094f
rtdl-yitonggang <rtdl-yitonggang@escmbc.com.cn>
2019-07-14 12:41:17 +0800

    NEW-190701  路由初建立

--- src/projects/filmApp/index.js
+++ src/projects/filmApp/index.js
@@ -1,2 +1,33 @@
 import React, { Component } from "react"
 import { route } from "react-router"
+import { directive } from "@babel/types";
+
+
+export default class IndexRouter extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    componentWillMount() {
+        // 监听路径变化
+
+    }
+
+    componentDidMount() {
+
+    }
+
+
+    render() {
+
+        return(
+            <div style={{ display: "flex", alignContent: "center", justifyContent: "center" }}>
+                <text>app路由页</text>
+
+            </div>
+        )
+    }
+}
